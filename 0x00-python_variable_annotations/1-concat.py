066e6a474335a7e260cd5b798d7fd7de05479a49
joekariuki3 <joekariuki3@gmail.com>
2024-01-11 12:24:52 +0300

    Basic annotation-floor

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""function that combines 2 strings"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """combine str1 and str2
+    and return str"""
+    return str1 + str2
