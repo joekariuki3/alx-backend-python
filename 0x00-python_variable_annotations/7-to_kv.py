e938f346525e7bcdf721185befefc112084c30a2
joekariuki3 <joekariuki3@gmail.com>
2024-01-11 14:40:51 +0300

    Complex types - string and int/float to tuple correction2

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -6,5 +6,6 @@ and returns a tuple(k: str, squre of v int or float)
 
 
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, Union[int, float]]:
-    """returns a tuple of k and v"""
+    """takes in k as string and v as either int or float,
+    then returns a tuple of k and v"""
     return (k, v * v)
