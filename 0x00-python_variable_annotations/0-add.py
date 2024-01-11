759227ccc5115abe5942656cf26b49d95be70b2d
joekariuki3 <joekariuki3@gmail.com>
2024-01-11 12:18:41 +0300

    Basic annotations - add

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""functipn to add 2 float"""
+
+
+def add(a: float, b: float) -> float:
+    """add sums up float a and float b and returns
+    a a float value"""
+    return a + b
