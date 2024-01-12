33efe676cc35dd091cb9965a552eb11244d6fe4e
joekariuki3 <joekariuki3@gmail.com>
2024-01-12 11:12:04 +0300

    Type Checking

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -1,21 +1,21 @@
 #!/usr/bin/env python3
 """type checking validation"""
 
-from typing import Tuple, Any, List
+from typing import Tuple, Any, List, Union
 """import required methods from typing module"""
 
 
-def zoom_array(lst: Tuple, factor: int = 2) -> Tuple:
+def zoom_array(lst: Tuple, factor: int = 2) -> List:
     """type checking validation"""
-    zoomed_in: Tuple = [
+    zoomed_in: List = [
         item for item in lst
         for i in range(factor)
     ]
     return zoomed_in
 
 
-array = [12, 72, 91]
+array = (12, 72, 91)
 
 zoom_2x = zoom_array(array)
 
-zoom_3x = zoom_array(array, 3.0)
+zoom_3x = zoom_array(array, 3)
