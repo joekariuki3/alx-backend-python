43f41184a2768a95c1b1909a59445dd00c52641e
joekariuki3 <joekariuki3@gmail.com>
2024-01-15 13:37:14 +0300

    Tasks add return value

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -5,7 +5,7 @@ import asyncio
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-def task_wait_random(max_dely: int):
+def task_wait_random(max_dely: int) -> asyncio.Task:
     """create a task and return it"""
     task1 = asyncio.create_task(wait_random(max_dely))
     return task1
