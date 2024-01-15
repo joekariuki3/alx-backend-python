c07f7f3bbf1621d770018fedef7273970acf52a8
joekariuki3 <joekariuki3@gmail.com>
2024-01-15 12:45:19 +0300

    Let's execute multiple coroutines at the same time with async correction

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -7,6 +7,8 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
+    """call wait_random n times returning a list of random
+    delay time"""
     delay_time_list = []
     for _ in range(n-n, n):
         delay_time = await wait_random(max_delay)
