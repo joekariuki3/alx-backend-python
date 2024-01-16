d842217d6b5c50acf508a936d911f14b86130414
joekariuki3 <joekariuki3@gmail.com>
2024-01-16 12:58:38 +0300

    Async Generator correction 3

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -2,9 +2,10 @@
 """Async Generator"""
 import asyncio
 import random
+from typing import AsyncGenerator
 
 
-async def async_generator():
+async def async_generator() -> AsyncGenerator:
     """ loop 10 times sleep for a second then yield
     a random number"""
     for _ in range(1, 11):
