1455bf3a9b44d5be906524494ef3668332af19d2
joekariuki3 <joekariuki3@gmail.com>
2024-01-16 12:41:38 +0300

    Async Generator

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""Async Generator"""
+import asyncio
+import random
+
+
+async def async_generator():
+    """ loop 10 times sleep for a second then yield
+    a random number"""
+    for i in range(1, 11):
+        await asyncio.sleep(1)
+        yield random.uniform(1, 10)
