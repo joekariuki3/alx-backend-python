20026c5fc78a91b350bb30f7b699229b5e691e6d
joekariuki3 <joekariuki3@gmail.com>
2024-02-02 12:54:53 +0300

    use parameterize in unit test for access_nested_map function in utils

---
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""TestAccessNestedMap class"""
+
+import unittest
+from utils import access_nested_map
+from typing import Sequence, Mapping, Any, Callable
+from parameterized import parameterized
+
+
+class TestAccessNestedMap(unittest.TestCase):
+    """test class for NestedMap in utils module"""
+
+    @parameterized.expand([
+        ({"a": 1}, ("a",), 1),
+        ({"a": {"b": 2}}, ("a",), {"b": 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2)
+        ])
+    def test_access_nested_map(self, nested_map, path, output):
+        """test case for output of access_nested_map"""
+        self.assertEqual(access_nested_map(nested_map, path), output)
