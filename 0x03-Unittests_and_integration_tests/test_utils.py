4a238d7cbb88f7317d88634e7b411db45d280d39
joekariuki3 <joekariuki3@gmail.com>
2024-02-02 14:57:52 +0300

    Use Mock and pactch to simumale Http Api call
    on utils.get_json function

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -2,7 +2,7 @@
 """TestAccessNestedMap class"""
 
 import unittest
-from utils import access_nested_map
+from utils import access_nested_map, get_json
 from typing import Sequence, Mapping, Any, Callable
 from parameterized import parameterized
 
@@ -27,3 +27,25 @@ class TestAccessNestedMap(unittest.TestCase):
         """test for KeyError exception"""
         with self.assertRaises(KeyError):
             access_nested_map(nested_map, path)
+
+
+class TestGetJson(unittest.TestCase):
+    """TestGetJson class implementation"""
+
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False})
+        ])
+    def test_get_json(self, test_url, test_payload):
+        """test get json call using mock using the request.get
+        method"""
+        with unittest.mock.patch("requests.get") as mget:
+            # asign return value as json cause get_json uses json()
+            # then on json() assign now the real data test_payload
+            mget.return_value.json.return_value = test_payload
+            # call get_json
+            response = get_json(test_url)
+            # make sure get_json was clalled with test_url
+            mget.assert_called_once_with(test_url)
+            # assert the call result and our expected result
+            self.assertEqual(response, test_payload)
