fce44790b55e98a73e2b27e357a09889c3c67f5a
joekariuki3 <joekariuki3@gmail.com>
2024-02-05 15:26:34 +0300

    test public repo

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -31,3 +31,24 @@ class TestGithubOrgClient(unittest.TestCase):
         new_company = GithubOrgClient("google")
         urls = new_company._public_repos_url
         self.assertEqual(urls, fake_url_list)
+
+    @patch("client.get_json")
+    def test_public_repos(self, mock_get_json):
+        """Test public_repos method in GithubOrgClient class"""
+        fake_dict = {"company": "google",
+                     "url":"https://api.github.com/orgs/google/repos",
+                     "license":{ "key": "mit"}}
+        mock_get_json.return_value = fake_dict
+
+        with patch.object(GithubOrgClient,
+                          "_public_repos_url",
+                          new_call=PropertyMock) as mock_public_urls:
+
+            fake_url_list = "https://api.github.com/orgs/google/repos"
+            mock_public_urls.return_value = fake_url_list
+            new_company = GithubOrgClient("google")
+            new_company.org()
+            url_list = new_company._public_repos_url()
+            public_repos = new_company.public_repos(license="mit")
+            mock_public_urls.assert_called_once_with()
+            mock_get_json.assert_called_once_with()
