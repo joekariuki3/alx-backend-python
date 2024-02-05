f53bf9de5e7bc7aafea6697b8efa57374aa5851d
joekariuki3 <joekariuki3@gmail.com>
2024-02-05 12:02:57 +0300

    Mocking a property

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -3,7 +3,7 @@
 
 import unittest
 from parameterized import parameterized
-from unittest.mock import patch
+from unittest.mock import patch, PropertyMock
 from client import GithubOrgClient
 
 
@@ -19,3 +19,15 @@ class TestGithubOrgClient(unittest.TestCase):
         new_company = GithubOrgClient(org_name)
         new_comp_info = new_company.org()
         mock_org.assert_called_once_with()
+
+    def test_public_repos_url(self):
+        """test for property method public urls"""
+        with patch.object(GithubOrgClient,
+                          "_public_repos_url",
+                          new_call=PropertyMock) as mock_public_urls:
+            fake_url_list = "https://api.github.com/orgs/google/repos"
+            mock_public_urls.return_value = fake_url_list
+
+        new_company = GithubOrgClient("google")
+        urls = new_company._public_repos_url
+        self.assertEqual(urls, fake_url_list)
