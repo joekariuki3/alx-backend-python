c7ab282bbff1215387e160692e949572c84dc87b
joekariuki3 <joekariuki3@gmail.com>
2024-02-05 11:21:12 +0300

    test_client using decorator patch mock

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -8,14 +8,14 @@ from client import GithubOrgClient
 
 
 class TestGithubOrgClient(unittest.TestCase):
-    
+
     @parameterized.expand([("google"), ("abc")])
-    @patch("client.get_json")
-    def test_org(self, org_name, mock_get_json):
+    @patch.object(GithubOrgClient, "org")
+    def test_org(self, org_name, mock_org):
+        url = f"https://api.github.com/orgs/{org_name}"
         fake_dict = {"company": org_name}
-        mock_get_json.return_value = fake_dict
+        mock_org.return_value = fake_dict
 
         new_company = GithubOrgClient(org_name)
         new_comp_info = new_company.org()
-        print(new_comp_info)
-        mock_get_json.assert_called_once_with(new_company.ORG_URL)
+        mock_org.assert_called_once_with()
