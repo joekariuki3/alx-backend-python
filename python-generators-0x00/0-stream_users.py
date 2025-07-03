276baeafe977ce639edb74e5f8cc3bd5ba7b7310
joekariuki3 <joekariuki3@gmail.com>
2025-07-03 10:45:15 +0300

    add stream_users generator to fetch user data from database

---
+++ python-generators-0x00/0-stream_users.py
@@ -0,0 +1,26 @@
+from mysql.connector import connect
+from dotenv import load_dotenv
+from os import getenv
+
+load_dotenv()
+
+
+def stream_users():
+    """a generator that fetches rows one by one from user_data table"""
+
+    connection = connect(
+        host = getenv("HOST"),
+        user = getenv("DBUSER", "root"),
+        password = getenv("PASSWORD"),
+        database = getenv("DBNAME", "ALX_prodev")
+    )
+
+    cursor = connection.cursor()
+
+    cursor.execute("SELECT * FROM user_data")
+
+    for row in cursor:
+        yield row
+
+    cursor.close()
+    connection.close()
