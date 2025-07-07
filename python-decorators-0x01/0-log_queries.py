30ca70ea4c3b202eded17422a32c9d85559dc423
joekariuki3 <joekariuki3@gmail.com>
2025-07-07 17:10:41 +0300

    fix: ensure correct wrapping of the log_queries decorator with functools.wraps

--- python-decorators-0x01/0-log_queries.py
+++ python-decorators-0x01/0-log_queries.py
@@ -8,6 +8,7 @@ def log_queries(func):
     decorator that logs sql query before executing it
     :param func: function to be decorated
     """
+    @functools.wraps(func)
     def wrapper(*args, **kwargs):
         query = args[0] if args else kwargs.get('query', None)
         if query:
