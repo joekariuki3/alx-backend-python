37c5869a20ac091750432a73a7bbe8d670644e61
joekariuki3 <joekariuki3@gmail.com>
2025-07-16 17:28:01 +0300

    Add custom User model and define Conversation and Message models

--- messaging_app/chats/models.py
+++ messaging_app/chats/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from uuid import uuid4
 
 
 class User(User):
@@ -7,8 +8,12 @@ class User(User):
     Custom User model that extends the default Django User model.
     """
 
-    # custom fields and methods goes here
-    pass
+    user_id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=128)
+    first_name = models.CharField(max_length=30, blank=True, null=True)
+    last_name = models.CharField(max_length=30, blank=True, null=True)
+    phone_number = models.CharField(max_length=15, blank=True, null=True)
 
 
 class Conversation(models.Model):
@@ -16,6 +21,7 @@ class Conversation(models.Model):
     Model that tracks which users are involved in a conversation.
     """
 
+    conversation_id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
     participants = models.ManyToManyField(User, related_name="conversations")
 
 
@@ -24,11 +30,13 @@ class Message(models.Model):
     Model containing the sender, receiver, and content of a message.
     """
 
+    message_id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
     sender = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name="sent_messages"
     )
     receiver = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name="received_messages"
     )
-    content = models.TextField()
-    timestamp = models.DateTimeField(auto_now_add=True)
+    message_body = models.TextField()
+    sent_at = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now_add=True)
