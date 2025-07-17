1f443883dcc039f7e27120b7e7de036a45c76e91
joekariuki3 <joekariuki3@gmail.com>
2025-07-17 17:30:16 +0300

    Fix variable name typo in chats app URL configuration

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -2,11 +2,11 @@ from django.urls import path, include
 from .views import ConversationViewSet, MessageViewSet
 from rest_framework.routers import DefaultRouter
 
-router = DefaultRouter()
-router.register('conversations', ConversationViewSet)
-router.register('messages', MessageViewSet)
+routers = DefaultRouter()
+routers.register('conversations', ConversationViewSet)
+routers.register('messages', MessageViewSet)
 
 
 urlpatterns = [
-    path('', include(router.urls)),
+    path('', include(routers.urls)),
 ]
