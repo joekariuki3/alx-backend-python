ec3e2b7a751f6756fed13195ed04cfd15bc79d4f
joekariuki3 <joekariuki3@gmail.com>
2025-07-17 17:16:07 +0300

    Refactor viewsets and update URLs to include chats app routes

--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -2,10 +2,10 @@ from rest_framework import viewsets
 from .models import Conversation, Message
 from .serializers import ConversationSerializer, MessageSerializer
 
-class ContactsViewSet(viewsets.ModelViewSet):
+class ConversationViewSet(viewsets.ModelViewSet):
     queryset = Conversation.objects.all()
     serializer_class = ConversationSerializer
 
-class MessagesViewSet(viewsets.ModelViewSet):
+class MessageViewSet(viewsets.ModelViewSet):
     queryset = Message.objects.all()
     serializer_class = MessageSerializer
