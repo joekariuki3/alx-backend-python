d9a5ebf6eb2cbf941588b259f32e7e4e2a8aefc6
joekariuki3 <joekariuki3@gmail.com>
2025-07-17 18:11:57 +0300

    Restrict access to conversations and messages to authenticated users and filter querysets by user context

--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -1,11 +1,19 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, status
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
 from .models import Conversation, Message
 from .serializers import ConversationSerializer, MessageSerializer
 
 class ConversationViewSet(viewsets.ModelViewSet):
-    queryset = Conversation.objects.all()
     serializer_class = ConversationSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return Conversation.objects.filter(users=self.request.user)
 
 class MessageViewSet(viewsets.ModelViewSet):
-    queryset = Message.objects.all()
     serializer_class = MessageSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return Message.objects.filter(conversation=self.kwargs['conversation_pk'])
