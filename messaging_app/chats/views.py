2bda43fa88aa780f8b7d14771d5e0146913ce877
joekariuki3 <joekariuki3@gmail.com>
2025-07-17 16:59:51 +0300

    Add viewsets for Conversation and Message models in chats app

--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Conversation, Message
+from .serializers import ConversationSerializer, MessageSerializer
 
-# Create your views here.
+class ContactsViewSet(viewsets.ModelViewSet):
+    queryset = Conversation.objects.all()
+    serializer_class = ConversationSerializer
+
+class MessagesViewSet(viewsets.ModelViewSet):
+    queryset = Message.objects.all()
+    serializer_class = MessageSerializer
