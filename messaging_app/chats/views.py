2c915ebbc11ebdb6461bba08fb399785d20f2963
joekariuki3 <joekariuki3@gmail.com>
2025-07-17 18:14:55 +0300

    Refactor viewsets in chats app: set static querysets and remove redundant user-specific filters

--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -1,19 +1,11 @@
-from rest_framework import viewsets, status
-from rest_framework.response import Response
-from rest_framework.permissions import IsAuthenticated
+from rest_framework import viewsets, status, filters
 from .models import Conversation, Message
 from .serializers import ConversationSerializer, MessageSerializer
 
 class ConversationViewSet(viewsets.ModelViewSet):
+    queryset = Conversation.objects.all()
     serializer_class = ConversationSerializer
-    permission_classes = [IsAuthenticated]
-
-    def get_queryset(self):
-        return Conversation.objects.filter(users=self.request.user)
 
 class MessageViewSet(viewsets.ModelViewSet):
+    queryset = Message.objects.all()
     serializer_class = MessageSerializer
-    permission_classes = [IsAuthenticated]
-
-    def get_queryset(self):
-        return Message.objects.filter(conversation=self.kwargs['conversation_pk'])
