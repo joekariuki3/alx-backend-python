ee6d786323f9f62412cfae94d1aaf616a75f3a49
joekariuki3 <joekariuki3@gmail.com>
2025-07-21 16:48:20 +0300

    Switch User model inheritance from User to AbstractUser for customization support

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
+    "rest_framework_simplejwt",
+    "chats"
 ]
 
 MIDDLEWARE = [
@@ -126,8 +128,12 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": [
         "rest_framework.authentication.SessionAuthentication",
+        "rest_framework_simplejwt.authentication.JWTAuthentication"
     ],
     "DEFAULT_PERMISSION_CLASSES": [
         "rest_framework.permissions.IsAuthenticated",
     ],
 }
+
+# use chats.models.user as user class
+AUTH_USER_MODEL = "chats.User"
