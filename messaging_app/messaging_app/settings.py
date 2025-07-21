8ea4821253b94f3803bac58e06e8afd849c11150
joekariuki3 <joekariuki3@gmail.com>
2025-07-21 23:15:20 +0300

    Add BasicAuthentication to REST framework authentication classes

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -127,6 +127,7 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 # REST Framework settings
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework.authentication.BasicAuthentication",
         "rest_framework.authentication.SessionAuthentication",
         "rest_framework_simplejwt.authentication.JWTAuthentication"
     ],
