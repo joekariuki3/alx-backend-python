01b87b455a98a7a030132b413f1eeef21ebdc203
joekariuki3 <joekariuki3@gmail.com>
2025-07-28 14:52:44 +0300

    Add pre_save signal to log message edits and track changes in MessageHistory

--- Django-signals_orm-0x04/messaging/signals.py
+++ Django-signals_orm-0x04/messaging/signals.py
@@ -1,8 +1,21 @@
 from django.dispatch import receiver
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, pre_save
 from .models import Message, Notification
+from Django-Chat.Models import MessageHistory
 
 @receiver(post_save, sender=Message)
 def send_notification_to_message_receiver(sender, instance, created, **kwargs):
     if created:
         Notification.objects.create(recipient=instance.receiver, message=instance)
+
+@receiver(pre_save, sender=Message)
+def log_message_edit(sender, instance, **kwargs):
+    if instance.pk:
+        old_message = Message.objects.get(pk=instance.pk)
+        if old_message.content != instance.content:
+            # log message
+            print(f"Message edited: {old_message.content} -> {instance.content}")
+            MessageHistory.objects.create(
+                edited=old_message.content,
+                message=old_message
+            )
