f6fea81dd1c3792c12bc5cf820d1d1bd65a09143
joekariuki3 <joekariuki3@gmail.com>
2025-07-28 13:35:02 +0300

    Add `Message` and `Notification` models to `messaging`: enable message handling and notifications storage.

--- Django-signals_orm-0x04/messaging/models.py
+++ Django-signals_orm-0x04/messaging/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Message(models.Model):
+    sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name='sender')
+    receiver = models.ForeignKey(User, on_delete=models.CASCADE, related_name='receiver')
+    content = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+class Notification(models.Model):
+    recipient = models.ForeignKey(User, on_delete=models.CASCADE, related_name='recipient')
+    message = models.ForeignKey(Message, on_delete=models.CASCADE, related_name='message')
+    seen = models.BooleanField(default=False)
+    timestamp = models.DateTimeField(auto_now_add=True)
